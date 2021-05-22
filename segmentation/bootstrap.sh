fb614b9edbf4a8defc5755a65dc0a761405e6590
Rayan Samy Ramoul <raysamram@gmail.com>
2021-05-22 15:06:13 +0200

    adding bootstrap sh

--- segmentation/bootstrap.sh
+++ segmentation/bootstrap.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+#SBATCH --mail-user=raysamram@gmail.com
+#SBATCH --mail-type=all
+#SBATCH --job-name=GPU-Embedding-Learning
+#SBATCH --output=/home/rramoul/convml_tt_gpu/Embedding.out
+#SBATCH --error=/home/rramoul/convml_tt_gpu/Embedding.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --gres-flags=enforce-binding
+
 python train_zooniverse_resized.py
